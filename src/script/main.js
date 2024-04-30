23a4398c1ab8cb43cc10cb203f0d6ed28a2e828c
fabricol <fabricioctnmiranda@gmail.com>
2024-04-30 10:05:57 -0300

    adição da contagem do tempo para o evento

--- src/script/main.js
+++ src/script/main.js
@@ -1,1 +1,27 @@
 AOS.init();
+
+const dataDoEvento = new Date("Dec 8, 2024 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function() {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+    const diasEmMS = 1000 * 60 * 60 * 24;
+    const horasEmMS = 1000 * 60 * 60;
+    const minutosEmMS = 1000 * 60;
+    const segundosEmMS = 1000;
+
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / diasEmMS);
+    const horasAteOEvento = Math.floor((distanciaAteOEvento % diasEmMS) / horasEmMS);
+    const minutosAteOEvento = Math.floor((distanciaAteOEvento % horasEmMS) / minutosEmMS);
+    const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutosEmMS) / segundosEmMS);
+
+    document.getElementById('contador').innerHTML = `${diasAteOEvento} dias ${horasAteOEvento} horas ${minutosAteOEvento} minutos e ${segundosAteOEvento} segundos.`
+    if (distanciaAteOEvento < 0) {
+        clearInterval (contaAsHoras);
+        document.getElementById('contador').innerHTML = 'Vendas encerradas'
+    }
+}, 1000);
